9c91cde24eafd725eecc3493b442b7c47945ef18
Andreas Achilleos <andreasachilleos1994@gmail.com>
2021-01-30 15:10:15 +0000

    completed the routes and controllers and started the views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+lemon = Ingredient.create(name: "lemon")
+ice = Ingredient.create(name: "ice")
+mint = Ingredient.create(name: "mint leaves")
+
+mojito = Cocktail.create(name: "Mojito")
+
+Dose.create(description: "one slice", cocktail: mojito, ingredient:lemon)
+Dose.create(description: "3 cubes", cocktail: mojito, ingredient:ice)
+Dose.create(description: "one leaf", cocktail: mojito, ingredient:mint)
