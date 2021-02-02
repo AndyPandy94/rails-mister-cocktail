605e2d7969d4cc0167ed47c5ad7869fdbaadfe55
Andreas Achilleos <andreasachilleos1994@gmail.com>
2021-02-02 20:30:34 +0000

    budnle installs and clourdinary

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,9 @@ mojito = Cocktail.create(name: "Mojito")
 Dose.create(description: "one slice", cocktail: mojito, ingredient:lemon)
 Dose.create(description: "3 cubes", cocktail: mojito, ingredient:ice)
 Dose.create(description: "one leaf", cocktail: mojito, ingredient:mint)
+
+require "open-uri"
+
+file = URI.open('https://giantbomb1.cbsistatic.com/uploads/original/9/99864/2419866-nes_console_set.png')
+cocktail = Cocktail.new(title: 'NES', body: "A great console")
+cocktail.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
